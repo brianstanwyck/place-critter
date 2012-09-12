e5e5fa8918acc5b9882717bc2e9cc538dc535d72
brianstanwyck <brian@highgroove.com>
2012-09-12 17:27:29 -0400

    OOify (kinda)

--- server.rb
+++ server.rb
@@ -9,6 +9,32 @@ require 'sass'
 FlickRaw.api_key= ENV['FLICKR_API_KEY']
 FlickRaw.shared_secret = ENV['FLICKR_API_SECRET']
 
+module PhotoSearch
+  def self.search(query)
+    flickr.photos.search(text: "#{query}", tags: "animal").to_a.map { |result| Photo.new(result) }
+  end
+end
+
+class Photo < Struct.new(:id)
+  def initialize(results)
+    super results["id"]
+  end
+end
+
+class SizedPhoto < Struct.new(:width, :height, :url)
+  def initialize(result)
+    width = result["width"].to_i
+    height = result["height"].to_i
+    url = result["source"]
+
+    super width, height, url
+  end
+
+  def self.all_sizes(photo)
+    flickr.photos.getSizes(photo_id: photo.id).to_a.map { |result| SizedPhoto.new(result) }
+  end
+end
+
 get '/' do
   slim :index
 end
@@ -28,19 +54,22 @@ get '/:width/:height/:query' do
   width = params[:width].to_i
   height = params[:height].to_i
 
-  flickr.photos.search(text: "#{query}", tags: "animal").to_a.shuffle.each do |photo|
-    @photo = flickr.photos.getSizes(photo_id: photo["id"]).sort_by do |size_response|
-      size_response["width"].to_i
-    end.find do |size_response|
-      size_response["width"].to_i >= width &&
-      size_response["height"].to_i >= height
+  PhotoSearch.search(query).shuffle.each do |photo|
+    sorted_photos = SizedPhoto.all_sizes(photo).sort_by do |sized_photo|
+      sized_photo.width
     end
+
+    @photo = sorted_photos.find do |sized_photo|
+      sized_photo.width >= width &&
+      sized_photo.height >= height
+    end
+
     break if @photo
   end
 
   if @photo
     content_type 'image/jpeg'
-    url = @photo["source"]
+    url = @photo.url
 
     image = open(url) do |f|
       Magick::Image.from_blob(f.read).first
